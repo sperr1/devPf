7c3af3e6c6f667b6788b0c94f856379520f5326a
Alex Sperr <sperr1@users.noreply.github.com>
2016-12-14 14:48:51 -0500

    Setting up eclipse, messing around with Java.

---
+++ Spring/src/springProj/hWorld.java
@@ -0,0 +1,12 @@
+package springProj;
+
+public class hWorld {
+
+	/*IT'S BEEN AGES SINCE I'VE DONE JAVA*/
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
